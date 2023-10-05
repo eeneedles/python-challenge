2c9d6d34ece16c26adccc416e0bb3d9c81c5a8f5
eeneedles <132238481+eeneedles@users.noreply.github.com>
2023-10-05 00:14:24 -0400

    added comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,8 @@
+#Import os and csv modules for creating file paths across operating systems and reading csv files
 import os
 import csv
 
+#Locate csv file and read using CSV module
 csvpath = os.path.join('Resources','budget_data.csv')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -14,6 +16,7 @@ with open(csvpath) as csvfile:
     changes = []
     previous = 0
 
+    #Calculate total number of months, net total amount of profit/losses over period
     for i,row in enumerate(csvreader):
         # print(row)
         # months = months + 1
@@ -21,6 +24,7 @@ with open(csvpath) as csvfile:
         profit_loss = int(row[1])
         net_total +=profit_loss
 
+        #Calculate changes in profit/losses over period
         if i > 0:
             
             change = profit_loss - previous
@@ -28,10 +32,10 @@ with open(csvpath) as csvfile:
             previous = profit_loss
         else:
             previous = profit_loss
-        
+    #Create financial analysis dataset displaying month total, net total of profit/losses, average change in profit/losses, and Greatest Increase and Decrease in Profits        
     print(months)
-    print(f"Net Total: ${net_total}")
-    
+    print(f"Total: ${net_total}")
+
     
 
 
